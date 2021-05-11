0f26657d36a9eb4280518fd9e000e4e54d46bed3
osyousufi <osyousufi@gmail.com>
2021-05-11 13:48:15 -0700

    first

---
+++ ui.js
@@ -0,0 +1,17 @@
+
+//index
+const whyUs = document.querySelector('#why-us');
+const benefits = document.querySelector('#benefits');
+
+ScrollReveal().reveal(whyUs, {delay: 250});
+ScrollReveal().reveal(benefits, {delay: 250});
+
+//Pricing
+const card1 = document.querySelector('#card-1');
+ScrollReveal().reveal(card1, {delay: 250});
+
+const card2 = document.querySelector('#card-2');
+ScrollReveal().reveal(card2, {delay: 500});
+
+const card3 = document.querySelector('#card-3');
+ScrollReveal().reveal(card3, {delay: 750});
